fd5a2ed4575bb7a99d31e5b92052ba16a70cccbe
Brian <dev@bashout.work>
2024-01-02 23:12:25 +0000

    init

---
+++ src/components/Intro.svelte
@@ -0,0 +1,5 @@
+<script>
+    let name = "Brian";
+</script>
+
+<h1>Welcome back {name}</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Intro from "../components/Intro.svelte";
+	import Summary from "../components/Summary.svelte";
+
+	let summaryRevealed: Boolean = false;
+
+	function toggleSummary() {
+		summaryRevealed = !summaryRevealed;
+	}
+
+</script>
+
+<Intro></Intro>
+
+<button on:click="{toggleSummary}">
+	Show summary
+</button>
+
+{#if summaryRevealed}
+	<Summary></Summary>
+{/if}
