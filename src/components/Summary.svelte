040161a713ce3d566a10a1b44ad14974e9d21d71
Brian <dev@bashout.work>
2024-01-06 02:01:24 +0000

    Fix summary list to render off store

--- src/components/Summary.svelte
+++ src/components/Summary.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-	import { Expenditure } from '../classes/Expenditure';
 	import { SpendCategory } from '../enums/SpendCategory';
+	import { expenditures } from '../stores';
 	import Pie from './charts/Pie.svelte';
-
-	let expenditures: Expenditure[] = [
-		new Expenditure(SpendCategory.Bill, 50, "Mortgage"),
-		new Expenditure(SpendCategory.Personal, 100, "Shoes"),
-		new Expenditure(SpendCategory.Work, 700, "Commuting")
-	];
 </script>
 
 <h1>Summary of expenditures</h1>
-<Pie data={expenditures} />
+<Pie data={$expenditures} />
 <table>
 	<th> Spend Category </th>
 	<th> Amount Spent </th>
 	<tbody>
-		{#each expenditures as expenditure}
+		{#each $expenditures as expenditure}
 			<tr>
 				<td>
 					{expenditure.type}
