00e5d3c386f96973624279c29f5f442693b4521c
Brian <dev@bashout.work>
2024-01-13 22:55:22 +0000

    center align content

--- src/components/Summary.svelte
+++ src/components/Summary.svelte
@@ -12,36 +12,44 @@
 	$: percentageOfIncome = ((totalExpenditure / $income) * 100).toFixed(2);
 </script>
 
-<h1>Summary of expenditures</h1>
-{#key $expenditures}
-	<Pie data={$groupedExpenditures} />
-	<div class="net-income">
-		<h2>Income</h2>
-		<h2>Net</h2>
-		<h2>Expenditures as percentage</h2>
-		<h2>£{$income}</h2>
-		<h2>£{netIncome}</h2>
-		<h2>{percentageOfIncome}%</h2>
-	</div>
-	<table>
-		<th> Spend Category </th>
-		<th> Amount Spent </th>
-		<tbody>
-			{#each $expenditures as expenditure}
-				<tr>
-					<td>
-						{expenditure.type}
-					</td>
-					<td>
-						{expenditure.amountSpent}
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-{/key}
+<div class="summary">
+	<h1>Summary of expenditures</h1>
+	{#key $expenditures}
+		<Pie data={$groupedExpenditures} />
+		<div class="net-income">
+			<h2>Income</h2>
+			<h2>Net</h2>
+			<h2>Expenditures as percentage</h2>
+			<h2>£{$income}</h2>
+			<h2>£{netIncome}</h2>
+			<h2>{percentageOfIncome}%</h2>
+		</div>
+		<table>
+			<th> Spend Category </th>
+			<th> Amount Spent </th>
+			<tbody>
+				{#each $expenditures as expenditure}
+					<tr>
+						<td>
+							{expenditure.type}
+						</td>
+						<td>
+							{expenditure.amountSpent}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/key}
+</div>
 
 <style>
+	.summary {
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+	}
+
 	.net-income {
 		background-color: rgb(250, 250, 250);
 		border-radius: 25px;
@@ -57,10 +65,4 @@
 	.net-income > * {
 		flex: 1 1 160px;
 	}
-
-	/* .net-module {
-		display: inline-flex;
-		padding-left: 10%;
-		padding-right: 10%;
-	} */
 </style>
