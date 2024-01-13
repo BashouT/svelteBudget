bdd99fcebb6fe27536c2c7a7fb7fbae81e876a3a
Brian <dev@bashout.work>
2024-01-13 23:07:25 +0000

    Basic styling

--- src/components/Summary.svelte
+++ src/components/Summary.svelte
@@ -16,30 +16,36 @@
 	<h1>Summary of expenditures</h1>
 	{#key $expenditures}
 		<Pie data={$groupedExpenditures} />
-		<div class="net-income">
+		<div class="summary-module">
 			<h2>Income</h2>
 			<h2>Net</h2>
 			<h2>Expenditures as percentage</h2>
-			<h2>£{$income}</h2>
-			<h2>£{netIncome}</h2>
-			<h2>{percentageOfIncome}%</h2>
+			<p>£{$income}</p>
+			<p>£{netIncome}</p>
+			<p>{percentageOfIncome}%</p>
 		</div>
-		<table>
-			<th> Spend Category </th>
-			<th> Amount Spent </th>
-			<tbody>
-				{#each $expenditures as expenditure}
-					<tr>
-						<td>
-							{expenditure.type}
-						</td>
-						<td>
-							{expenditure.amountSpent}
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+		<div class="summary-module">
+			<table>
+				<th><h2>Spend Category</h2> </th>
+				<th><h2>Amount Spent</h2> </th>
+				<th><h2>Description</h2> </th>
+				<tbody>
+					{#each $expenditures as expenditure}
+						<tr>
+							<td>
+								{expenditure.type}
+							</td>
+							<td>
+								{expenditure.amountSpent}
+							</td>
+							<td>
+								{expenditure.description}
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
 	{/key}
 </div>
 
@@ -50,7 +56,7 @@
 		align-items: center;
 	}
 
-	.net-income {
+	.summary-module {
 		background-color: rgb(250, 250, 250);
 		border-radius: 25px;
 		text-align: center;
@@ -60,9 +66,11 @@
 		justify-content: center;
 		align-items: center;
 		flex-wrap: wrap;
+		margin-bottom: 20px;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 	}
 
-	.net-income > * {
+	.summary-module > * {
 		flex: 1 1 160px;
 	}
 </style>
